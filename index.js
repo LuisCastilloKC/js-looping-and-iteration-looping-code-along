171b2ca027770dba520d5ac11b022c12b62d457c
Luis Castillo <mr.luiscastillo@gmail.com>
2021-06-17 21:08:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+
+function writeCards(cards, event) {
+    const cardNameArr = []
+    for (let i = 0; i < cards.length; i++) {
+        cardNameArr[i] = (`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+    }
+    return cardNameArr;
+}
+
+let number = 10
+
+function countDown(number) {
+    while (number >= 0){
+        console.log(number--);
+    }
+}
